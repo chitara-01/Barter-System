2d36f074beb151593133c3db57793f0d2a50789b
chitara-01 <chitara.1@iitj.ac.in>
2020-12-06 20:00:25 +0530

    xyz

--- barter_system/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, url_for
-from barter_system import app
-from barter_system.models import User, Post
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return "<h1>Welcome to Barter System!</h1>"
